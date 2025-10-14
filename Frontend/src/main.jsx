ac1de8559a553b65d32166fc851a7f73dedd0883
Caio Sousa Mello <cmcaiomello@gmail.com>
2025-10-14 14:45:55 -0300

    clientId

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import Navegacao from "./routes.jsx";
 import { GoogleOAuthProvider } from "@react-oauth/google";
 
+const clientId = "SEU_CLIENT_ID.apps.googleusercontent.com";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
