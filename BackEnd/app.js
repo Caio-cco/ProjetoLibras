74702ae36425c30c2381bfbe9a182d3783d734c6
Caio <cmcaiomello@gmail.com>
2025-10-22 10:39:43 -0300

    mudança da rota do react sem motivo, FAVOR NÃO MUDAR MAIS

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -5,7 +5,7 @@ import loginEndpoints from "./controller/loginController.js";
 const app = express();
 const PORT = 5010;
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "http://localhost:5174", credentials: true }));
 app.use(express.json());
 app.use("/", loginEndpoints);
 
