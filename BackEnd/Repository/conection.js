c4df425859b1b2e2fad10894bda15aad17e7f9f9
Caio Sousa Mello <cmcaiomello@gmail.com>
2025-10-28 21:49:16 -0300

    local funcionando

--- BackEnd/Repository/conection.js
+++ BackEnd/Repository/conection.js
@@ -4,6 +4,7 @@ const conection = await mysql.createConnection({
   host: "falar-e-magico.cry2muqqwntr.sa-east-1.rds.amazonaws.com",
   user: "Caio",
   password: "06171825csm",
+  database: "tcc",
   multipleStatements: true 
 });
 
