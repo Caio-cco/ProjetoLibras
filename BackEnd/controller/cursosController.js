dcd4dc92f2233abce5e51bc675474b4b0820f496
Caio <cmcaiomello@gmail.com>
2025-11-03 09:13:03 -0300

    .

--- BackEnd/controller/cursosController.js
+++ BackEnd/controller/cursosController.js
@@ -1,14 +1,16 @@
 import * as repoCursos from '../Repository/cursosRepository.js';
 import { Router } from 'express';
+import { getAuthentication } from '../utils/jwt.js';
 
 const endpoints = Router();
+const autenticador = getAuthentication();
 
-endpoints.get('/cursos/progresso', async (req, resp) => {
+endpoints.get('/cursos/progresso', autenticador, async (req, resp) => {
     let registros = await repoCursos.verificarProgresso();
     resp.send(registros);
 })
 
-endpoints.get('/dificuldades', async (req, resp) => {
+endpoints.get('/dificuldades', autenticador, async (req, resp) => {
     let dif = await repoCursos.dificuldadeTabela();
     resp.send ({ dif });
 })
