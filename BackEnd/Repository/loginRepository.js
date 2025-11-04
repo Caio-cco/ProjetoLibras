ffc86f450ab7a61f80fdac9ba30e05fa2abc646e
Caio <cmcaiomello@gmail.com>
2025-11-04 10:31:02 -0300

    pequenas mudanças

--- BackEnd/Repository/loginRepository.js
+++ BackEnd/Repository/loginRepository.js
@@ -3,42 +3,42 @@ import { conection } from './conection.js';
 
 export async function validarCredenciais(email, senha) {
 
-  const [rows] = await conection.execute(
-    'SELECT * FROM usuario WHERE email = ? AND senha = MD5(?)',
-    [email, senha]
-  );
-  return rows[0] || null;
+  const [rows] = await conection.execute(
+    'SELECT * FROM usuario WHERE email = ? AND senha = MD5(?)',
+    [email, senha]
+  );
+  return rows[0] || null;
 }
 
 
 export async function criarConta({ email, senha, name }) {
 
-  const [result] = await conection.execute(
-    'INSERT INTO usuario (email, senha, nome, login_social) VALUES (?, MD5(?), ?, 0)',
-    [email, senha, name]
-  );
-  return result.insertId;
+  const [result] = await conection.execute(
+    'INSERT INTO usuario (email, senha, nome, login_social) VALUES (?, MD5(?), ?, 0)',
+    [email, senha, name]
+  );
+  return result.insertId;
 }
 
 
 export async function upsertUsuarioSocial({ email, name, picture }) {
-  const [rows] = await conection.execute('SELECT * FROM usuario WHERE email = ?', [email]);
-  const usuarioExistente = rows[0];
+  const [rows] = await conection.execute('SELECT * FROM usuario WHERE email = ?', [email]);
+  const usuarioExistente = rows[0];
 
-  if (usuarioExistente) {
+  if (usuarioExistente) {
 
-    await conection.execute(
-      'UPDATE usuario SET nome = ?, foto_url = ?, login_social = 1 WHERE email = ?',
-      [name, picture, email]
-    );
+    await conection.execute(
+      'UPDATE usuario SET nome = ?, foto_url = ?, login_social = 1 WHERE email = ?',
+      [name, picture, email]
+    );
 
-    return { ...usuarioExistente, nome: name, foto_url: picture, login_social: 1 };
-  } else {
+    return { ...usuarioExistente, nome: name, foto_url: picture, login_social: 1 };
+  } else {
 
-    const [result] = await conection.execute(
-      'INSERT INTO usuario (email, nome, foto_url, senha, login_social) VALUES (?, ?, ?, NULL, 1)',
-      [email, name, picture]
-    );
+    const [result] = await conection.execute(
+      'INSERT INTO usuario (email, nome, foto_url, senha, login_social) VALUES (?, ?, ?, NULL, 1)',
+      [email, name, picture]
+    );
 
     return { id: result.insertId, email, nome: name, foto_url: picture, senha: null, login_social: 1 };
  }
