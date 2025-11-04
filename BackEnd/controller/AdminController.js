93f54e3026cb2eb5bd41b4bd51e622a1dcbb4858
Caio <cmcaiomello@gmail.com>
2025-11-04 11:15:29 -0300

    resolvendo problemas da api

--- BackEnd/controller/AdminController.js
+++ BackEnd/controller/AdminController.js
@@ -1,4 +1,4 @@
-import { cadastro } from '../Repository/cadastroRepository.js';
+import { cadastro } from '../Repository/AdminRepository.js';
 import { Router } from 'express';
 
 const api = Router();
--- BackEnd/rotas.js
+++ BackEnd/rotas.js
@@ -1,11 +1,11 @@
-import cadastroController from './controller/cadastroController.js'
+import AdminController from './controller/AdminController.js'
 import loginController from './controller/loginController.js'
 import cursosController from './controller/cursosController.js'
 import usuarioController from './controller/usuarioController.js'
 import express from 'express';
 
 export function adicionarRotas(api) {
-    api.use(cadastroController)
+    api.use(AdminController)
     api.use(loginController)
     api.use(cursosController)
     api.use(usuarioController)
